5f8e41b33d2663033337c4a33ad496860f5125a3
Vicki Dudley <36187395+mutatedneutrino@users.noreply.github.com>
2020-11-10 16:38:11 +0000

    reorderded test queries to make sense

--- Steam Recommendation/Database/SQL Stuff/test_queries.py
+++ Steam Recommendation/Database/SQL Stuff/test_queries.py
@@ -4,13 +4,13 @@ import Database_Queries
 #select = 'TOP 10 *'
 #table = 'dbo.SteamRecommendations'
 
+#Database_Queries.select(select,table)
+
 #JOIN QUERY
 select = 'TOP 100 dbo.SteamRecommendations.NAME, dbo.GameSales.Genre, dbo.GameSales.Year'
 table1 = 'dbo.SteamRecommendations'
 table2 = 'dbo.GameSales'
 join_type = 'RIGHT OUTER JOIN'
 join_property = 'dbo.GameSales.Name=dbo.SteamRecommendations.NAME'
-
-#Database_Queries.select(select,table)
 
 Database_Queries.join(select,table1,table2,join_type,join_property)
